51e5684fdf3bc3ce220855a4bff16332dccaa528
Jasmine T <tanghocle456@gmail.com>
2023-07-25 20:31:58 -0500

    Refactoring

--- lua/plugins/lsp_zero.lua
+++ lua/plugins/lsp_zero.lua
@@ -16,5 +16,52 @@ return {
         { 'hrsh7th/nvim-cmp' },     -- Required
         { 'hrsh7th/cmp-nvim-lsp' }, -- Required
         { 'L3MON4D3/LuaSnip' },     -- Required
-    }
+    },
+
+
+    config = function()
+        local lsp = require('lsp-zero').preset({})
+
+        lsp.on_attach(function(client, bufnr)
+            lsp.default_keymaps({ buffer = bufnr })
+        end)
+        -- Fix Undefined global 'vim'
+        require("mason-lspconfig").setup {
+            ensure_installed = {
+                "lua_ls",
+                "rust_analyzer",
+
+                "pyright",
+
+                "clangd",
+                "cmake",
+
+
+
+                "golangci_lint_ls",
+                "gopls",
+
+            },
+        }
+        require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
+
+        lsp.setup()
+
+        local cmp = require("cmp")
+        require("luasnip.loaders.from_vscode").lazy_load()
+
+        cmp.setup({
+            sources = {
+                { name = "path" },
+                { name = "nvim_lsp_signature_help" },
+                { name = "nvim_lsp" },
+                { name = "nvim_lua" },
+                { name = "buffer",                 keyword_length = 3 },
+                { name = "luasnip",                keyword_length = 2 },
+            },
+            mapping = {
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }
+        })
+    end
 }
